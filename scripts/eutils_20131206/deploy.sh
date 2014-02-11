150c5a181abb4b4d747a4f5860734a5a9939da1e
renaud <renaud@apache.org>
2014-02-11 11:33:52 +0100

    m

--- scripts/eutils_20131206/deploy.sh
+++ scripts/eutils_20131206/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#https://pubchemdb.googlecode.com/svn-history/r32/trunk/PubChemDB/PubChemDB-apps/src/repository/gov/nih/nlm/ncbi/ncbi-eutils-axis2/2.0/ncbi-eutils-axis2-2.0.pom
+#ftp://ftp.ncbi.nlm.nih.gov/pub/eutils/soap/v2.0/java/current/
 
 FILE=eutils_axis2.jar
 GROUP_ID=gov.nih.nlm.ncbi
@@ -9,27 +9,12 @@ VERSION=20131206
 
 REPO=snapshot-repo
 URL=file:/Volumes/HDD2/ren_data/dev_hdd/sources/maven_repo_googlecode/snapshots/
-#REMOTE_URL=https://github.com/renaud/maven_repo/raw/master/snapshots
+REMOTE_URL=https://ren-maven-repo.googlecode.com/git/snapshots
 
 mvn deploy:deploy-file -Dfile=$FILE  -DgroupId=$GROUP_ID -DartifactId=$ARTIFACT_ID -Dversion=$VERSION -Dpackaging=jar -DrepositoryId=$REPO -Durl=$URL
 
 echo '\n\n<dependency>\n<groupId>'$GROUP_ID'</groupId>\n<artifactId>'$ARTIFACT_ID'</artifactId>\n<version>'$VERSION'</version>\n</dependency>\n\n'
-#echo '\n<repositories>\n<repository>\n<id>'$REPO'</id>\n<url>'$REMOTE_URL'</url>\n</repository>\n</repositories>\n\n'
+echo '\n<repositories>\n<repository>\n<id>'$REPO'</id>\n<url>'$REMOTE_URL'</url>\n</repository>\n</repositories>\n\n'
 
 echo 'cd to '$URL
 
-
-# <dependency>
-# <groupId>gov.nih.nlm.ncbi</groupId>
-# <artifactId>ncbi-eutils-axis2-2.0</artifactId>
-# <version>2.0</version>
-# </dependency>
-
-
-
-# <repositories>
-# <repository>
-# <id>snapshot-repo</id>
-# <url>https://github.com/renaud/maven_repo/raw/master/snapshots</url>
-# </repository>
-# </repositories>
